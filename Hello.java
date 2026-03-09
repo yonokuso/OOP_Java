9215e9375dd822fe7c11f58e5560d48f9d264113
Usagi <qltenwkd@naver.com>
2026-03-09 17:40:10 +0900

    Initialize

---
+++ Hello.java
@@ -0,0 +1,17 @@
+
+public class Hello{
+    public class void main(String[] args){
+        // 인사 출력
+        System.out.println("Hello, java World!");
+    }
+}
+
+
+
+// public class Main {
+
+
+
+    
+//     return 0;
+// }
