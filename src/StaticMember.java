716880a86eafc44ec8a78406922106850d583a5e
Usagi <qltenwkd@naver.com>
2026-04-28 14:49:02 +0900

    static, static 환율 계산기

---
+++ src/StaticMember.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+class CurrencyConverter
+{
+    private static double rate;
+    public static double toDollar(double won)
+    {
+        return won/rate; // 변환
+    }
+
+    public static double toKWR(double dollar)
+    {
+        return dollar * rate; // 변환
+    }
+
+    public static void setRate(double r)
+    {
+        rate = r; // 환율 설정
+    }
+}
+
+public class StaticMember
+{
+    public static void main(String[] args)
+    {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("환율(1달러)>> ");
+        double rate = scanner.nextDouble();
+        CurrencyConverter.setRate(rate);
+        System.out.println("백만원은 $" + CurrencyConverter.toDollar(1000000)     +"입니다.");
+        System.out.println("$100는 " + CurrencyConverter.toKWR(100)    + "원입니다.");
+        scanner.close();
+    }
+}
