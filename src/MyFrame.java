791e07bf074e7afc085e990c30e6d671a2423cf0
Usagi <qltenwkd@naver.com>
2026-05-18 16:43:22 +0900

    JFrame Container

---
+++ src/MyFrame.java
@@ -0,0 +1,16 @@
+import javax.swing.*;
+
+public class MyFrame extends JFrame
+{
+    public MyFrame()
+    {
+        setTitle("300x300 스윙 프레임 만들기");
+        setSize(300,300);
+        setVisible(true);
+    }
+
+    public static void main(String[] args)
+    {
+        MyFrame frame = new MyFrame();
+    }
+}
